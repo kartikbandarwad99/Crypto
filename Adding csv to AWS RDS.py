4bd4fe49a961d08dbc1dccd100492a789eb31d7e
kartikbandarwad99 <82508149+kartikbandarwad99@users.noreply.github.com>
2022-01-21 18:05:27 +0530

    Update Adding csv to AWS RDS.py

--- Adding csv to AWS RDS.py
+++ Adding csv to AWS RDS.py
@@ -11,25 +11,8 @@ database='#'
 # read the csv file
 df=pd.read_csv('my_crypto_investments.csv')
 df['Date']=pd.to_datetime(df['Date'], format='%d-%m-%Y')
-# establish a database connection
-conn=pymysql.connect(user=username,password=password,host=host,database=database)
-cursor=conn.cursor()
 
 def insert_data(table_name,df,username=username,password=password,host=host,database=database,conn=conn,cursor=cursor):
-    # changing python datatypes to sql datatypes
-    dtypes_sql={
-    'object':'varchar',
-    'int64':'int',
-    'float64':'float',
-    'datetime64[ns]':'date',
-    'timedelta64[ns]':'varchar'
-    }
-    col_str=", ".join("{} {}({})".format(n,d,50) if d=='varchar' else "{} {}".format(n,d) for n,d in zip(df.columns,df.dtypes.replace(dtypes_sql)))
-    # create a table if it doesn't exist
-    cursor.execute("drop table if exists %s;" %(table_name))
-    cursor.execute("create table %s(%s)" %(table_name,col_str))
-    cursor.connection.commit()
-    # copy the csv file to the database
     engine = create_engine("mysql+pymysql://{user}:{pw}@crypto.cfeucnwsd8ed.us-east-1.rds.amazonaws.com/{db}"
                            .format(user=username, pw=password, db=database))
     df.to_sql('{}'.format(table_name), con = engine, if_exists = 'append', chunksize = 1000,index=False)
