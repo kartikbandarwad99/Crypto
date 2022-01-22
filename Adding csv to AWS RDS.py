e6ab7fde9844cf49c5b4320dd75685175380c803
kartikbandarwad99 <82508149+kartikbandarwad99@users.noreply.github.com>
2022-01-22 13:44:34 +0530

    Update Adding csv to AWS RDS.py

--- Adding csv to AWS RDS.py
+++ Adding csv to AWS RDS.py
@@ -12,13 +12,13 @@ database='#'
 df=pd.read_csv('my_crypto_investments.csv')
 df['Date']=pd.to_datetime(df['Date'], format='%d-%m-%Y')
 
-def insert_data(table_name,df,username=username,password=password,host=host,database=database,conn=conn,cursor=cursor):
-    engine = create_engine("mysql+pymysql://{user}:{pw}@crypto.cfeucnwsd8ed.us-east-1.rds.amazonaws.com/{db}"
+def insert_data(table_name,df,username=username,password=password,host=host,database=database):
+    conn = create_engine("mysql+pymysql://{user}:{pw}@crypto.cfeucnwsd8ed.us-east-1.rds.amazonaws.com/{db}"
                            .format(user=username, pw=password, db=database))
-    df.to_sql('{}'.format(table_name), con = engine, if_exists = 'append', chunksize = 1000,index=False)
+    df.to_sql('{}'.format(table_name), con = conn, if_exists = 'append', chunksize = 1000,index=False)
     # return statement to check if the table is inserted properly
     return pd.read_sql('select * from %s' %(table_name),con=conn)
 
 # define the table name and call the insert_data to insert a csv file into the database
-tbl='crypto_inv'
-insert_data('{}'.format(tbl),df)
+#table_name='dataframe'
+#insert_data('{}'.format(table_name),df)
